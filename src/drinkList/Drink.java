d46c4b5aae299758b9e98c32ca28399381107e59
leedongyeop <ldy_1204@naver.com>
2022-10-01 16:58:37 +0900

    directory : drink-list

---
+++ src/drinkList/Drink.java
@@ -0,0 +1,11 @@
+package drinkList;
+
+public abstract class Drink {
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
