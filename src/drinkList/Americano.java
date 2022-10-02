81b9ff266c3251cf847243f6b9d6c1bf5bfb8f2d
leedongyeop <ldy_1204@naver.com>
2022-10-03 00:24:44 +0900

    Drink를 상속받는 아메리카노

---
+++ src/drinkList/Americano.java
@@ -0,0 +1,12 @@
+package drinkList;
+
+public class Americano extends Drink {
+    public Americano() {
+        description = "시~원한 아메리카노";
+    }
+
+    @Override
+    public double cost() {
+        return 5.0;
+    }
+}
