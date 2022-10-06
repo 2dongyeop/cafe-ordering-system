b0218bc3477249154156fe559e04b899002d8a80
leedongyeop <ldy_1204@naver.com>
2022-10-06 23:03:50 +0900

    cup size 선택 옵션 추가

--- src/drinkList/Drink.java
+++ src/drinkList/Drink.java
@@ -2,9 +2,14 @@ package drinkList;
 
 public abstract class Drink {
     String description;
+    String size;
 
     public String getDescription() {
         return description;
+    }
+
+    public String getSize() {
+        return size;
     }
 
     public abstract double cost();
