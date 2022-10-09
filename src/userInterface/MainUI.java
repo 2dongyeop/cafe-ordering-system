2bbb18ce64292dc78cf082d3fbb6bbe37dce1666
leedongyeop <ldy_1204@naver.com>
2022-10-09 21:57:34 +0900

    메뉴 선택 과정을 MainUI로부터 분리

--- src/userInterface/MainUI.java
+++ src/userInterface/MainUI.java
@@ -6,6 +6,7 @@ import drinkFactory.SizeFactory;
 import drinkList.Drink;
 import userInterface.applicationException.InvalidInputException;
 import userInterface.applicationException.SameIdException;
+import userInterface.orderProcess.OrderProcess;
 import userInterface.userAuthentication.AuthProcess;
 
 import java.io.BufferedReader;
@@ -23,6 +24,7 @@ public class MainUI {
     DrinkFactory drinkFactory;
     OptionFactory optionFactory;
     AuthProcess userAuthProcess;
+    OrderProcess orderProcess;
     boolean isExited = false;
 
     public MainUI() {
@@ -48,12 +50,14 @@ public class MainUI {
             System.out.println("=====카페 주문 시스템이 시작됩니다.=====");
             System.out.println("1. 음료 주문 | 2. 이달의 추천 메뉴 보기 | 3. 종료");
 
-            int menuSelect = Integer.parseInt(br.readLine());
 
             try {
+                int menuSelect = Integer.parseInt(br.readLine());
+
                 switch (menuSelect) {
                     case 1:
                         orderProcess();
+
                         printOrderDetails((ArrayList<Drink>) orderList);
                         break;
                     case 2:
@@ -76,59 +80,8 @@ public class MainUI {
         /**
          * 템플릿 메소드
          */
-        selectDrink();
-        selectCupSize();
-        selectOptions();
+        orderProcess = new OrderProcess();
         askForAdditionalOrder();
-    }
-
-    private final void selectDrink() throws IOException, InvalidInputException {
-        System.out.println("=====음료 주문이 시작됩니다.=====");
-        System.out.println("1. 아메리카노 | 2. 카페라떼 | 3. 레몬차 | 4. 아인슈페너");
-
-        try {
-            int drinkSelect = Integer.parseInt(br.readLine());
-            /**
-             * 팩토리 패턴을 이용한 음료 생성
-             */
-            drink = drinkFactory.createDrink(drinkSelect);
-        } catch (ClassCastException e) {
-            System.out.println("입력은 정수만 가능합니다.");
-        }
-    }
-
-    private final void selectCupSize() throws IOException, InvalidInputException {
-        System.out.println("=====음료 사이즈를 선택하세요.=====");
-        System.out.println("1. Small | 2. Medium | 3. Large");
-
-        try {
-            int sizeSelect = Integer.parseInt(br.readLine());
-
-            drink = sizeFactory.selectSize(sizeSelect, drink);
-        } catch (ClassCastException e) {
-            System.out.println("입력은 정수만 가능합니다.");
-        }
-    }
-
-    private final void selectOptions() throws IOException, InvalidInputException {
-        boolean isEnded = false;
-        //옵션은 여러 번을 추가할 수 있다.
-        do {
-            System.out.println("=====추가 옵션을 선택하세요.=====");
-            System.out.println("1. Ice | 2. Shot | 3. Syrup | 4. [Do Not add Option]");
-
-            try {
-                int sizeSelect = Integer.parseInt(br.readLine()); //ClassCastException
-
-                if (sizeSelect > 0 && sizeSelect < 5) {
-                    drink = optionFactory.selectOption(sizeSelect, drink);
-                    if (sizeSelect == 4) isEnded = true;
-                } else
-                    throw new InvalidInputException("1부터 4까지만 입력 가능합니다.");
-            } catch (ClassCastException e) {
-                System.out.println("입력은 정수만 가능합니다.");
-            }
-        } while (!isEnded);
     }
 
     private final void askForAdditionalOrder() throws IOException {
@@ -139,9 +92,11 @@ public class MainUI {
             int addOrderSelect = Integer.parseInt(br.readLine());
 
             if (addOrderSelect == 1) {
+                drink = orderProcess.getDrink();
                 orderList.add(drink);
 
             } else if (addOrderSelect == 2) {
+                drink = orderProcess.getDrink();
                 orderList.add(drink);
                 isExited = true;
             } else {
@@ -162,6 +117,11 @@ public class MainUI {
     }
 
     private final void showRecommendedMenu() {
-
+        /**
+         * 추천 메뉴를 보여줍니다.
+         * 추천 메뉴 선택 방식은 아래와 같이 고민해봐야 합니다.
+         * 1. 랜덤하게 메뉴를 고르기
+         * 2. 누적판매량을 기반으로 고르기
+         */
     }
 }
---
+++ src/userInterface/orderProcess/OrderProcess.java
@@ -0,0 +1,78 @@
+package userInterface.orderProcess;
+
+import drinkFactory.DrinkFactory;
+import drinkFactory.OptionFactory;
+import drinkFactory.SizeFactory;
+import drinkList.Drink;
+import userInterface.applicationException.InvalidInputException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OrderProcess {
+    Drink drink;
+    SizeFactory sizeFactory = new SizeFactory();
+    DrinkFactory drinkFactory = new DrinkFactory();
+    OptionFactory optionFactory = new OptionFactory();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public OrderProcess() throws InvalidInputException, IOException {
+        selectDrink();
+        selectCupSize();
+        selectOptions();
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    private final void selectDrink() throws IOException, InvalidInputException {
+        System.out.println("=====음료 주문이 시작됩니다.=====");
+        System.out.println("1. 아메리카노 | 2. 카페라떼 | 3. 레몬차 | 4. 아인슈페너");
+
+        try {
+            int drinkSelect = Integer.parseInt(br.readLine());
+            /**
+             * 팩토리 패턴을 이용한 음료 생성
+             */
+            drink = drinkFactory.createDrink(drinkSelect);
+        } catch (ClassCastException e) {
+            System.out.println("입력은 정수만 가능합니다.");
+        }
+    }
+
+    private final void selectCupSize() throws IOException, InvalidInputException {
+        System.out.println("=====음료 사이즈를 선택하세요.=====");
+        System.out.println("1. Small | 2. Medium | 3. Large");
+
+        try {
+            int sizeSelect = Integer.parseInt(br.readLine());
+
+            drink = sizeFactory.selectSize(sizeSelect, drink);
+        } catch (ClassCastException e) {
+            System.out.println("입력은 정수만 가능합니다.");
+        }
+    }
+
+    private final void selectOptions() throws IOException, InvalidInputException {
+        boolean isEnded = false;
+        //옵션은 여러 번을 추가할 수 있다.
+        do {
+            System.out.println("=====추가 옵션을 선택하세요.=====");
+            System.out.println("1. Ice | 2. Shot | 3. Syrup | 4. [Do Not add Option]");
+
+            try {
+                int sizeSelect = Integer.parseInt(br.readLine()); //ClassCastException
+
+                if (sizeSelect > 0 && sizeSelect < 5) {
+                    drink = optionFactory.selectOption(sizeSelect, drink);
+                    if (sizeSelect == 4) isEnded = true;
+                } else
+                    throw new InvalidInputException("1부터 4까지만 입력 가능합니다.");
+            } catch (ClassCastException e) {
+                System.out.println("입력은 정수만 가능합니다.");
+            }
+        } while (!isEnded);
+    }
+}
