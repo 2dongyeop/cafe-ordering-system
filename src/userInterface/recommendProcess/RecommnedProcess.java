94aba19fdc4a45a5a0c1cf711643a42d8c3a8b12
leedongyeop <ldy_1204@naver.com>
2022-10-11 22:50:58 +0900

    추천 메뉴를 정하는 Process

---
+++ src/userInterface/recommendProcess/RecommnedProcess.java
@@ -0,0 +1,27 @@
+package userInterface.recommendProcess;
+
+import drinkFactory.DrinkFactory;
+import drinkList.Drink;
+import userInterface.applicationException.InvalidInputException;
+
+import java.util.Random;
+
+
+public class RecommnedProcess {
+    Drink recommendedDrink;
+    DrinkFactory drinkFactory;
+
+    public RecommnedProcess() throws InvalidInputException {
+        drinkFactory = new DrinkFactory();
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(3) + 1;
+        recommendedDrink = drinkFactory.createDrink(randomNumber);
+    }
+
+    public Drink getRecommendedDrink() {
+        return recommendedDrink;
+    }
+
+
+}
