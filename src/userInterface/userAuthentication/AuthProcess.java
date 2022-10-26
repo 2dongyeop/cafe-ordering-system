c5caea3de1ff0daa541cfce96539103d49ca70f3
leedongyeop <ldy_1204@naver.com>
2022-10-26 19:53:48 +0900

    switch expression 적용

--- src/userInterface/userAuthentication/AuthProcess.java
+++ src/userInterface/userAuthentication/AuthProcess.java
@@ -8,6 +8,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+enum OrderSelect {
+    MEMBERORDER(1),
+    NONMEMBERSELECT(2);
+
+    private int selectNum;
+
+    OrderSelect(int n) {
+        this.selectNum = n;
+    }
+
+    int getSelectNum() {
+        return selectNum;
+    }
+}
+
 public class AuthProcess {
     private boolean logInSuccess = false;
     private static Set<User> userDB;
@@ -20,18 +35,14 @@ public class AuthProcess {
         System.out.println("=====어서오세요. 22세기 빽다방에 오신걸 환영합니다^^.=====");
         System.out.println("=====1. 회원으로 주문하기 | 2. 비회원으로 주문하기");
 
+
         try {
             int inputOrderMethod = Integer.parseInt(br.readLine());
-
+            
             switch (inputOrderMethod) {
-                case 1:
-                    memberOrder();
-                    break;
-                case 2:
-                    nonMemberOrder();
-                    break;
-                default:
-                    throw new InvalidInputException("선택지는 1 또는 2만 가능합니다.");
+                case 1 -> memberOrder();
+                case 2 -> nonMemberOrder();
+                default -> throw new InvalidInputException("선택지는 1 또는 2만 가능합니다.");
             }
         } catch (ClassCastException e) {
             System.out.println("입력은 정수만 가능합니다.");
@@ -48,14 +59,9 @@ public class AuthProcess {
                 int inputAuthMethod = Integer.parseInt(br.readLine());
 
                 switch (inputAuthMethod) {
-                    case 1:
-                        signUp();
-                        break;
-                    case 2:
-                        logInSuccess = logIn();
-                        break;
-                    default:
-                        throw new InvalidInputException("선택지는 1 또는 2만 가능합니다.");
+                    case 1 -> signUp();
+                    case 2 -> logInSuccess = logIn();
+                    default -> throw new InvalidInputException("선택지는 1 또는 2만 가능합니다.");
                 }
             } catch (ClassCastException e) {
                 System.out.println("입력은 정수만 가능합니다.");
@@ -80,17 +86,16 @@ public class AuthProcess {
             userDB.add(new User(signUpId, signUpPassword));
             System.out.println("회원가입 성공!");
         } else {
-            System.out.println("Id 중복 : 동일한 id가 있습니다.");
-            return;
+            throw new SameIdException("Id 중복 : 동일한 id가 있습니다.");
         }
     }
 
     private final boolean hasSameIdCheck(final String id) {
         /**
          * iterator 반복자를 이용한 id 중복 체크
-         */
-        Iterator<User> iterator = userDB.iterator();
-        while (iterator.hasNext()) {
+         */                                             //메소드에 final 다 붙일거면 클래스에다 붙여라
+        Iterator<User> iterator = userDB.iterator();   //userDB를 HashMap으로 id를 키로 잡고,
+        while (iterator.hasNext()) {                    //BufferedReader를 싱글턴으로만들고, 입력값을 enum으로 바꾸는 과정을 만들어보기
             User temp = iterator.next();
 
             if (id.equals(temp.getId()))
